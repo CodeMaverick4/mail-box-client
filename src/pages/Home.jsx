24e977a65293f881313108937d228ef24d5d035b
Burhan Ahmed <burhan.ahmed@advantal.net>
2025-09-16 11:06:43 +0530

    update the home page with react boostrap.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
 
 export default function Home() {
-  return (
-    <div>
-      home
-    </div>
+  return (    
+     <Container className="mt-2 ">
+      <h3 className="mb-3">Welcome to Mail Box!!!</h3>
+      <hr />
+    </Container>
   )
 }
